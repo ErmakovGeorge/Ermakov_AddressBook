8981e6538283262e15e4d9cc64ece9972bfdbad2
George <george.ermakov95@gmail.com>
2024-02-26 14:36:11 -0800

    AddressBookTest class created

---
+++ src/test/java/org/example/AddressBookTest.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddressBookTest {
+
+    @Test
+    void addEntry() {
+    }
+
+    @Test
+    void removeEntry() {
+    }
+
+    @Test
+    void findEntry() {
+    }
+
+    @Test
+    void listEntries() {
+    }
+}
