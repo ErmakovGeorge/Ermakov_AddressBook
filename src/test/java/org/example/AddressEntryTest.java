f30294f21efbfb5154f80648601cd1719e750054
George <george.ermakov95@gmail.com>
2024-02-26 14:23:09 -0800

    created test & updated Maven.xml file for testing

---
+++ src/test/java/org/example/AddressEntryTest.java
@@ -0,0 +1,74 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AddressEntryTest {
+
+    @org.junit.jupiter.api.Test
+    void getFirstName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setFirstName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getLastName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setLastName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getStreet() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setStreet() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getCity() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setCity() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getState() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setState() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getZip() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setZip() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getEmail() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setEmail() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getPhone() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setPhone() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToString() {
+    }
+}
